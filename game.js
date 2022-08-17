f34604588f4cdeb7af8c43a4488cace9135ac49e
austinc45 <austinc6783@gmail.com>
2022-08-17 14:27:34 -0400

    rename

--- game.js
+++ game.js
@@ -1,80 +1,98 @@
-let inputEczema = document.querySelector('#buttonEczema')
-let inputAcne = document.querySelector('#buttonAcne')
-let inputHerpes = document.querySelector('#buttonHerpes')
-let inputScabbies = document.querySelector('#buttonScabbies')
-let inputMonkey = docuemnt.querySelector('#buttonMonkey')
-let count = 0
-let imageSkin = document.querySelector('#image')
-let answer
-let correct 
-let pDiv = document.querySelector('#resource')
-let nextQ = document.querySelector('#next')
-let paragraph = document.querySelector('#incorrect')
-let noneP = docuemnt.querySelector('#none')
-inputEczema.onclick = function (){
-    answer = eczema 
-    if (answer == array2[count]){
-        console.log('correct')
-        correct ++
-    }else{    
-      paragraph.innerHTML = wrong[count] 
-    
-    }
-}
-inputAcne.onclick = function (){
-    answer = acne 
-    if (answer == array2[count]){
-        console.log('correct')
-        correct ++
-    }else{    
-      paragraph.innerHTML = wrong[count] 
-    
-    }
-}
-inputHerpes.onclick = function (){
-    answer = herpes 
-    if (answer == array2[count]){
-        console.log('correct')
-        correct ++
-    }else{    
-      paragraph.innerHTML = wrong[count] 
-     
-    }
-}
-inputMonkey.onclick = function (){
-    answer = monkeypox
-    if (answer == array2[count]){
-        console.log('correct')
-        correct ++
-    }else{    
-      paragraph.innerHTML = wrong[count] 
-     
-    }
-}
-inputScabbies.onclick = function (){
-    answer = scabbies 
-    if (answer == array2[count]){
-        console.log('correct')
-        correct ++
-    }else{    
-      paragraph.innerHTML = wrong[count] 
-      
-    }
-}
-nextQ.onclick = function(){
-    paragraph.innerHTML = ''
-    count ++
-    imageSkin.src = array[count]
-    if (count == array.length){
-        noneP.style.display = 'none'
-        let end = document.createElement('p')
-        end.innerHTML = 'your score was based on luck'
-        body.appendChild(end)
-    }
-}
+let inputEczema = document.querySelector("#buttonEczema");
+let inputAcne = document.querySelector("#buttonAcne");
+let inputHerpes = document.querySelector("#buttonHerpes");
+let inputScabbies = document.querySelector("#buttonScabbies");
+let inputMonkey = document.querySelector("#buttonMonkey");
+let inputMolluscum = document.querySelector("#buttonM");
+let count = 0;
+let imageSkin = document.querySelector("#image");
+let answer;
+let correct;
+let pDiv = document.querySelector("#resource");
+let nextQ = document.querySelector("#next");
+let paragraph = document.querySelector("#incorrect");
+let noneP = document.querySelector("#none");
+let array = [
+  "images/acne1.png",
+  "images/monkeypox1.jpg",
+  "images/scabies.jpg",
+  "images/MolluscumC1.jpg",
+  "images/monkeypox2.jpg",
+];
+let array2 = [
+  "acne",
+  "monkeypox",
+  "scabbies",
+  "Molluscum contagiosum",
+  "monkeypox",
+];
+let wrong = ["wrong", "wrong", "wrong", "wrong", "wrong"];
+let quizEnd = document.querySelector("#finish");
 
-
-// let array = [images, images, images];
-// let array2 = [answers, answers, answer];
-//let wrong  = [wrong, wrong, wrong]
-
+inputEczema.onclick = function () {
+  answer = "eczema";
+  if (answer == array2[count]) {
+    console.log("correct");
+    correct++;
+  } else {
+    paragraph.innerHTML = wrong[count];
+  }
+};
+inputAcne.onclick = function () {
+  answer = "acne";
+  if (answer == array2[count]) {
+    console.log("correct");
+    correct++;
+  } else {
+    paragraph.innerHTML = wrong[count];
+  }
+};
+inputHerpes.onclick = function () {
+  answer = "herpes";
+  if (answer == array2[count]) {
+    console.log("correct");
+    correct++;
+  } else {
+    paragraph.innerHTML = wrong[count];
+  }
+};
+inputMonkey.onclick = function () {
+  answer = "monkeypox";
+  if (answer == array2[count]) {
+    console.log("correct");
+    correct++;
+  } else {
+    paragraph.innerHTML = wrong[count];
+  }
+};
+inputScabbies.onclick = function () {
+  answer = "scabbies";
+  if (answer == array2[count]) {
+    console.log("correct");
+    correct++;
+  } else {
+    paragraph.innerHTML = wrong[count];
+  }
+};
+inputMolluscum.onclick = function () {
+  answer = "Molluscum contagiosum";
+  if (answer == array2[count]) {
+    console.log("correct");
+    correct++;
+  } else {
+    paragraph.innerHTML = wrong[count];
+  }
+};
+nextQ.onclick = function () {
+  paragraph.innerHTML = "";
+  count++;
+  imageSkin.src = array[count];
+  if (count == array.length) {
+    noneP.style.display = "none";
+    let end = document.createElement("p");
+    end.innerHTML = "your score was based on luck";
+    quizEnd.appendChild(end);
+    console.log(end)
+    console.log(quizEnd)
+  }
+};
